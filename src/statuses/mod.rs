c7437e88150e68e4979a1f4089d076159c92ffa4
Max Thimmig <mthimm2@uic.edu>
2023-04-14 09:38:31 -0500

    write test for volatile status types

--- src/statuses/mod.rs
+++ src/statuses/mod.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum NonVolatileStatusType {
     Freeze(u8),
     Paralysis,
@@ -122,4 +122,168 @@ fn test_non_volatile_status() {
 }
 
 #[test]
-fn test_volatile_status() {}
+fn test_volatile_status() {
+    // All volatile status types
+    let mut bound: Option<VolatileStatusType> = Some(VolatileStatusType::Bound(1));
+    let mut confusion: Option<VolatileStatusType> = Some(VolatileStatusType::Confusion(1));
+    let flinch: Option<VolatileStatusType> = Some(VolatileStatusType::Flinch);
+    let seeded: Option<VolatileStatusType> = Some(VolatileStatusType::Seeded);
+    let mut rampage: Option<VolatileStatusType> = Some(VolatileStatusType::Rampage(1));
+    let mut charging: Option<VolatileStatusType> = Some(VolatileStatusType::Charging(1));
+    let mut recharging: Option<VolatileStatusType> = Some(VolatileStatusType::Recharging(1));
+
+    // BOUND TEST
+    match bound {
+        Some(VolatileStatusType::Bound(bind_count)) => {
+            assert_eq!(bound, Some(VolatileStatusType::Bound(1)));
+            assert_eq!(bind_count, 1);
+            bound = Some(VolatileStatusType::Bound(2));
+            assert_eq!(bound, Some(VolatileStatusType::Bound(2)));
+        }
+        _ => panic!("Bind test failed"),
+    }
+    assert_ne!(bound, confusion);
+    assert_ne!(bound, flinch);
+    assert_ne!(bound, seeded);
+    assert_ne!(bound, rampage);
+    assert_ne!(bound, charging);
+    assert_ne!(bound, recharging);
+    assert_ne!(bound, Some(VolatileStatusType::Confusion(1)));
+    assert_ne!(bound, Some(VolatileStatusType::Flinch));
+    assert_ne!(bound, Some(VolatileStatusType::Seeded));
+    assert_ne!(bound, Some(VolatileStatusType::Rampage(38)));
+    assert_ne!(bound, Some(VolatileStatusType::Charging(1)));
+    assert_ne!(bound, Some(VolatileStatusType::Recharging(1)));
+
+    // CONFUSION TEST
+    match confusion {
+        Some(VolatileStatusType::Confusion(confusion_count)) => {
+            assert_eq!(confusion, Some(VolatileStatusType::Confusion(1)));
+            assert_eq!(confusion_count, 1);
+            confusion = Some(VolatileStatusType::Confusion(2));
+            assert_eq!(confusion, Some(VolatileStatusType::Confusion(2)));
+        }
+        _ => panic!("Confusion test failed"),
+    }
+    assert_ne!(confusion, bound);
+    assert_ne!(confusion, flinch);
+    assert_ne!(confusion, seeded);
+    assert_ne!(confusion, rampage);
+    assert_ne!(confusion, charging);
+    assert_ne!(confusion, recharging);
+    assert_ne!(confusion, Some(VolatileStatusType::Bound(1)));
+    assert_ne!(confusion, Some(VolatileStatusType::Flinch));
+    assert_ne!(confusion, Some(VolatileStatusType::Seeded));
+    assert_ne!(confusion, Some(VolatileStatusType::Rampage(3)));
+    assert_ne!(confusion, Some(VolatileStatusType::Charging(1)));
+    assert_ne!(confusion, Some(VolatileStatusType::Recharging(1)));
+
+    // FLINCH TEST
+    match flinch {
+        Some(VolatileStatusType::Flinch) => {
+            assert_eq!(flinch, Some(VolatileStatusType::Flinch));
+        }
+        _ => panic!("Flinch test failed"),
+    }
+    assert_ne!(flinch, bound);
+    assert_ne!(flinch, confusion);
+    assert_ne!(flinch, seeded);
+    assert_ne!(flinch, rampage);
+    assert_ne!(flinch, charging);
+    assert_ne!(flinch, recharging);
+    assert_ne!(flinch, Some(VolatileStatusType::Bound(1)));
+    assert_ne!(flinch, Some(VolatileStatusType::Confusion(1)));
+    assert_ne!(flinch, Some(VolatileStatusType::Seeded));
+    assert_ne!(flinch, Some(VolatileStatusType::Rampage(3)));
+    assert_ne!(flinch, Some(VolatileStatusType::Charging(1)));
+    assert_ne!(flinch, Some(VolatileStatusType::Recharging(1)));
+
+    // SEEDED TEST
+    match seeded {
+        Some(VolatileStatusType::Seeded) => {
+            assert_eq!(seeded, Some(VolatileStatusType::Seeded));
+        }
+        _ => panic!("Seeded test failed"),
+    }
+    assert_ne!(seeded, bound);
+    assert_ne!(seeded, confusion);
+    assert_ne!(seeded, flinch);
+    assert_ne!(seeded, rampage);
+    assert_ne!(seeded, charging);
+    assert_ne!(seeded, recharging);
+    assert_ne!(seeded, Some(VolatileStatusType::Bound(1)));
+    assert_ne!(seeded, Some(VolatileStatusType::Confusion(1)));
+    assert_ne!(seeded, Some(VolatileStatusType::Flinch));
+    assert_ne!(seeded, Some(VolatileStatusType::Rampage(3)));
+    assert_ne!(seeded, Some(VolatileStatusType::Charging(1)));
+    assert_ne!(seeded, Some(VolatileStatusType::Recharging(1)));
+
+    // RAMPAGE TEST
+    match rampage {
+        Some(VolatileStatusType::Rampage(rampage_count)) => {
+            assert_eq!(rampage, Some(VolatileStatusType::Rampage(1)));
+            assert_eq!(rampage_count, 1);
+            rampage = Some(VolatileStatusType::Rampage(2));
+            assert_eq!(rampage, Some(VolatileStatusType::Rampage(2)));
+        }
+        _ => panic!("Rampage test failed"),
+    }
+    assert_ne!(rampage, bound);
+    assert_ne!(rampage, flinch);
+    assert_ne!(rampage, seeded);
+    assert_ne!(rampage, confusion);
+    assert_ne!(rampage, charging);
+    assert_ne!(rampage, recharging);
+    assert_ne!(rampage, Some(VolatileStatusType::Bound(1)));
+    assert_ne!(rampage, Some(VolatileStatusType::Flinch));
+    assert_ne!(rampage, Some(VolatileStatusType::Seeded));
+    assert_ne!(rampage, Some(VolatileStatusType::Confusion(3)));
+    assert_ne!(rampage, Some(VolatileStatusType::Charging(1)));
+    assert_ne!(rampage, Some(VolatileStatusType::Recharging(1)));
+
+    // CHARGING TEST
+    match charging {
+        Some(VolatileStatusType::Charging(charge_count)) => {
+            assert_eq!(charging, Some(VolatileStatusType::Charging(1)));
+            assert_eq!(charge_count, 1);
+            charging = Some(VolatileStatusType::Charging(2));
+            assert_eq!(charging, Some(VolatileStatusType::Charging(2)));
+        }
+        _ => panic!("Charging test failed"),
+    }
+    assert_ne!(charging, bound);
+    assert_ne!(charging, flinch);
+    assert_ne!(charging, seeded);
+    assert_ne!(charging, confusion);
+    assert_ne!(charging, rampage);
+    assert_ne!(charging, recharging);
+    assert_ne!(charging, Some(VolatileStatusType::Bound(1)));
+    assert_ne!(charging, Some(VolatileStatusType::Flinch));
+    assert_ne!(charging, Some(VolatileStatusType::Seeded));
+    assert_ne!(charging, Some(VolatileStatusType::Confusion(3)));
+    assert_ne!(charging, Some(VolatileStatusType::Rampage(1)));
+    assert_ne!(charging, Some(VolatileStatusType::Recharging(1)));
+
+    // RECHARGING TEST
+    match recharging {
+        Some(VolatileStatusType::Recharging(recharge_count)) => {
+            assert_eq!(recharging, Some(VolatileStatusType::Recharging(1)));
+            assert_eq!(recharge_count, 1);
+            recharging = Some(VolatileStatusType::Recharging(2));
+            assert_eq!(recharging, Some(VolatileStatusType::Recharging(2)));
+        }
+        _ => panic!("Recharging test failed"),
+    }
+    assert_ne!(recharging, bound);
+    assert_ne!(recharging, flinch);
+    assert_ne!(recharging, seeded);
+    assert_ne!(recharging, confusion);
+    assert_ne!(recharging, rampage);
+    assert_ne!(recharging, charging);
+    assert_ne!(recharging, Some(VolatileStatusType::Bound(1)));
+    assert_ne!(recharging, Some(VolatileStatusType::Flinch));
+    assert_ne!(recharging, Some(VolatileStatusType::Seeded));
+    assert_ne!(recharging, Some(VolatileStatusType::Confusion(3)));
+    assert_ne!(recharging, Some(VolatileStatusType::Rampage(1)));
+    assert_ne!(recharging, Some(VolatileStatusType::Charging(1)));
+}
